1733addeedc18ebc3353d976085e17ab2d7aa85c
osieltorres <osiel@worky.mx>
2022-01-31 13:45:57 -0600

    feat(src): Procesos

--- src/pages/disfruta-el-proceso/index.js
+++ src/pages/disfruta-el-proceso/index.js
@@ -10,7 +10,20 @@ const Home = () => {
     <SectionDivider divider />
     <SectionTitle>Ama el proceso</SectionTitle>
     <SectionText>
-      
+      En 1961 el artista Robert Morris publicó una obra titulada "Una caja con el sonido de su propia creación". Esta obra consiste en una caja de madera con una bocina por dentro emitiendo
+      sonidos de personas trabajando con madera.
+
+      Lo que Morris buscaba expresar con esta obra es que la belleza del arte no se encuentra en el producto final, en la caja, sino en su proceso de creación; la gente
+      trabajando con la madera.
+
+      Erróneamente apreciamos nuestras obras solo por su resultado y nos olvidamos del trabajo que hubo detrás de él y de su contexto.
+
+      Bajo esta premisa, el construir una nueva funcionalidad solo es una experiencia agradable cuando se termina, y no cuando se está elaborando.
+
+      Esto quiere decir que, como diseñadores, programadores y dueños de producto, la mayor parte de nuestro tiempo estamos esperando a que ocurra ese momento para poder disfrutar nuestro trabajo.
+
+      Sin embargo, si cambiamos la perspectiva y entendemos que la belleza, no solo de nuestras obras sino de la vida, está en los aprendizajes y experiencias obtenidas en cada proyecto y no en su fin o su resultado,
+      entonces tendremos un trabajo y una vida más placentera, pues gozaremos a diario de nuestros procesos y seremos más pacientes con nuestros resultados.
     </SectionText>
     </Section>
     </Layout>
